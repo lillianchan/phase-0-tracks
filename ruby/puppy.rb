a2f3d8ce8f81ef39d8491a5a140c3fac7697a91d
elillian <elillian@gmail.com>
2016-07-31 06:28:45 -0700

    added puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,40 @@
+
+#Release 0: Design a Class
+
+I would design a Puppy class like below. 
+Create a class, add some methods for the
+class to do. 
+Design driver code for instance methods.
+
+SPECIES---------------
+Puppies Adorableus
+
+CHARACTERISTICS--------
+Friendliness: 10/10
+Activity level: very active 9/10
+Coat color: red brown
+Gender: Male
+
+BEHAVIOR-----------
+Hug
+Snuggle
+Drool
+Snore 
+
+class Puppy
+	def fetch
+		puts "I fetched the ball."
+	end
+	def speak
+		puts "woof"
+	end
+end
+
+A Puppy class may have the following characteristics and behavior 
+such as speak, fetch, tricks, etc.
+
+Puppy.methods
+Puppy.fetch
+Puppy.speak 
+
+
